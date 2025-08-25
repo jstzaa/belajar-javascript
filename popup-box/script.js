2b071e9baf72db000ea8e0a1101e70cb2159ff11
jstzaa <fahrizakurniawan51@gmail.com>
2025-08-25 10:16:27 +0700

    belajar javascript dasar

---
+++ popup-box/script.js
@@ -0,0 +1,10 @@
+/*
+alert('Hello World') // Digunakan untuk menampilkan popup biasa
+prompt('Masukkan nama Anda') // Digunakan untuk menampilkan popup input
+confirm('Selamat Anda telah login') // Digunakan untuk menampilkan popup konfirmasi
+*/
+
+alert('halo selamat datang')
+let nama = prompt('masukkan nama')
+
+alert('nama kamu ' + nama)
---
+++ variabel-tipe-data/script.js
@@ -0,0 +1,5 @@
+var x = 12 // Deklarasi x sebagai variabel, dan 12 sebagai nilai atau isi dari variabel js dengan tipe data number(integer)
+let y = 'testing' // Deklarasi y sebagai variabel, dan testing sebagai nilai atau isi dari variabel js dengan tipe data string
+const z = true // Deklarasi z sebagai variabel konstan(tidak dapat ditimpa), dan true sebagai nilai atau isi dari variabel js dengan tipe data boolean
+
+console.log(x + y + z) //console.log untuk menampilkan variabel di console
