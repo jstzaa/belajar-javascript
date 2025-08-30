033ea2d9ad72c46f2cd82826213c5836d55c3827
jstzaa <fahrizakurniawan51@gmail.com>
2025-08-30 18:05:30 +0700

    belajar while loop(praktik)

--- while-loop/script.js
+++ while-loop/script.js
@@ -1,6 +1,14 @@
 /*
 Syntax:
-while(kondisi){
+while(kondisi terminasi){
     aksi
+    increment/decrement
 }
 */
+
+let angkot = 1
+
+while(angkot <= 10){
+    console.log('Angkot No. ' + angkot + 'beroperasi dengan baik.')
+    angkot++
+}
