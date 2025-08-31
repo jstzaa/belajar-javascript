59c5bb7b71958cc8add3f51a3ce46ea8e89e9d5a
jstzaa <fahrizakurniawan51@gmail.com>
2025-08-31 09:37:15 +0700

    menambahkan komentar pada program while angkot

--- while-loop/script.js
+++ while-loop/script.js
@@ -6,9 +6,9 @@ while(kondisi terminasi){
 }
 */
 
-let angkot = 1
+let angkot = 1 //variabel(nilai awal)
 
-while(angkot <= 10){
-    console.log('Angkot No. ' + angkot + 'beroperasi dengan baik.')
-    angkot++
+while(angkot <= 10){ //kondisi terminasi
+    console.log('Angkot No. ' + angkot + 'beroperasi dengan baik.') //aksi
+    angkot++ //increment
 }
