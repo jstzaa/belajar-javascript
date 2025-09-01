0ba688cb4ea16368b6a8fefad55ef35664eb92b0
jstzaa <fahrizakurniawan51@gmail.com>
2025-09-01 17:29:26 +0700

    belajar else if

---
+++ else-if/script.js
@@ -0,0 +1,19 @@
+/*
+syntax:
+if(kondisi){
+    aksi1(jika kondisi benar)
+}else{
+    aksi2(jika kondisi salah)
+}
+*/
+let jmlAngkot = 10
+
+for(let noAngkot=1;noAngkot<=jmlAngkot;noAngkot++){
+    if(noAngkot <= 6){
+        console.log('Angkot No. ' + noAngkot + ' beroperasi dengan baik.')
+    }else if(noAngkot === 8){
+        console.log('Angkot No. ' + noAngkot + ' sedang lembur.')
+    }else{
+        console.log('Angkot No. ' + noAngkot + ' sedang tidak beroperasi.')
+    }
+}
