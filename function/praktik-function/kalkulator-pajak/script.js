9928c9f5bbe2105f5f27893c98bf257293c6ceb1
jstzaa <fahrizakurniawan51@gmail.com>
2025-09-07 13:06:53 +0700

    melakukan perbaikan pada fungsi hitungPajak

--- function/praktik-function/kalkulator-pajak/script.js
+++ function/praktik-function/kalkulator-pajak/script.js
@@ -1,9 +1,15 @@
 alert('Selamat datang di aplikasi penghitung pajak')
 let nilaiAwal = parseInt(prompt('Masukkan nilai awal '))
-let presentasePajak = parseInt(prompt('Masukkan presentase pajak '))
+let persentasePajak = parseInt(prompt('Masukkan presentase pajak '))
 
-function hitungPajak(nilaiAwal, presentasePajak) {
-    return nilaiAwal + (nilaiAwal * (presentasePajak / 100))
+function hitungPajak(nilaiAwal, persentasePajak) {
+    let hasil
+    if (isNaN(nilaiAwal) || isNaN(persentasePajak)){
+        hasil = 'input salah'
+    } else {
+        hasil = nilaiAwal + (nilaiAwal * (persentasePajak / 100))
+    }
+    return hasil
 }
 
-alert('Nilai akhir pajak kamu: ' + hitungPajak(nilaiAwal, presentasePajak))
+alert('Nilai akhir pajak kamu: ' + hitungPajak(nilaiAwal, persentasePajak))
