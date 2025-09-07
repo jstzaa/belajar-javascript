be89b7f611b7c1519e4d0b427d47dbf93ef894b0
jstzaa <fahrizakurniawan51@gmail.com>
2025-09-07 12:52:23 +0700

    refactoring kalkulator pajak

--- function/praktik-function/kalkulator-pajak/script.js
+++ function/praktik-function/kalkulator-pajak/script.js
@@ -1,11 +1,9 @@
 alert('Selamat datang di aplikasi penghitung pajak')
+let nilaiAwal = parseInt(prompt('Masukkan nilai awal '))
+let presentasePajak = parseInt(prompt('Masukkan presentase pajak '))
 
 function hitungPajak(nilaiAwal, presentasePajak) {
-    nilaiAwal = parseInt(prompt('Masukkan nilai awal '))
-    presentasePajak = parseInt(prompt('Masukkan presentase pajak '))
-    let nilaiPajak = nilaiAwal * (presentasePajak / 100)
-
-    return nilaiAwal + nilaiPajak
+    return nilaiAwal + (nilaiAwal * (presentasePajak / 100))
 }
 
-alert('Nilai akhir pajak kamu: ' + hitungPajak())
+alert('Nilai akhir pajak kamu: ' + hitungPajak(nilaiAwal, presentasePajak))
