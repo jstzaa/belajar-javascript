4af08d8fac3aca26e2004f43007b35503af1aa08
jstzaa <fahrizakurniawan51@gmail.com>
2025-09-12 17:35:12 +0700

    belajar array

---
+++ array/script.js
@@ -0,0 +1,16 @@
+/*
+syntax:
+// deklarasi array
+let namaArray = ['nilai1','nilai2','nilai3'] 
+||
+let namaArray = []
+namaArray = ['nilai1','nilai2','nilai3']
+
+// memanggil array
+console.log(namaArray[index])
+* index dimulai dari 0
+*/
+
+let hewan = ['kucing','kuda','sapi','koala','gajah']
+
+console.table(hewan)
