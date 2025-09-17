bea905414cf37289859ebc78bcdf228542c3bc1e
jstzaa <fahrizakurniawan51@gmail.com>
2025-09-17 19:50:03 +0700

    mengubah background color random dengan javascript dom

--- javascript-DOM/praktik-dom/mengubah-warna/script.js
+++ javascript-DOM/praktik-dom/mengubah-warna/script.js
@@ -8,3 +8,18 @@ const changeBgColor = document.getElementById('changeColor')
 changeBgColor.addEventListener('click', function(){
     body.classList.toggle('color')
 })
+
+// tambahkan tombol baru
+const newButton = document.createElement('button')
+const isiNewButton = document.createTextNode('UBAH WARNA RANDOM')
+newButton.appendChild(isiNewButton)
+body.appendChild(newButton)
+
+// tambahkan event ubah warna random
+newButton.addEventListener('click', function(){
+    const r = Math.floor(Math.random() * 255)
+    const g = Math.floor(Math.random() * 255)
+    const b = Math.floor(Math.random() * 255)
+
+    body.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b  + ')'
+})
