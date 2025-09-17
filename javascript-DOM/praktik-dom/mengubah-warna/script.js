fa87311536ba5fec4460bf3dc11b3303665fbbba
jstzaa <fahrizakurniawan51@gmail.com>
2025-09-17 20:42:10 +0700

    mengubah background color menggunakan slider rgb dengan javascript dom

--- javascript-DOM/praktik-dom/mengubah-warna/script.js
+++ javascript-DOM/praktik-dom/mengubah-warna/script.js
@@ -1,3 +1,4 @@
+// 1. mengubah warna dengan toggle
 // memanggil tag body
 const body = document.getElementsByTagName('body')[0]
 
@@ -9,6 +10,8 @@ changeBgColor.addEventListener('click', function(){
     body.classList.toggle('color')
 })
 
+
+// 2. mengubah warna random
 // tambahkan tombol baru
 const newButton = document.createElement('button')
 const isiNewButton = document.createTextNode('UBAH WARNA RANDOM')
@@ -23,3 +26,18 @@ newButton.addEventListener('click', function(){
 
     body.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b  + ')'
 })
+
+
+// mengubah warna dengan slider
+// memanggil slider
+const slider = document.querySelectorAll('input')
+
+for (let i = 0; i < slider.length; i++){
+    slider[i].addEventListener('input', function(){
+    const r = slider[0].value
+    const g = slider[1].value
+    const b = slider[2].value
+
+    body.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')'
+})
+}
