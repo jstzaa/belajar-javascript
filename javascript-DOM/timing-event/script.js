fc0842a022bfcda4e2f84fb5a9b97150c61bbcd7
jstzaa <fahrizakurniawan51@gmail.com>
2025-09-19 10:35:23 +0700

    belajar timing event

---
+++ javascript-DOM/timing-event/script.js
@@ -0,0 +1,15 @@
+/*
+1. setTimeout() // menjalankan program setelah beberapa saat
+2. setInterval() // menjalankan program berulang kali dengan interval
+*/
+
+// setTimeout()
+setTimeout(function(){
+    console.log('Hello World')
+}, 2000)
+
+// setInterval()
+setInterval(function(){
+    console.log("Hello World")
+}, 1000)
+
